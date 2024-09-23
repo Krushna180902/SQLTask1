a8d65abc8f7b87110848d7bea0476b561db7073e
Krushna180902 <krushnasanjay18@gmail.com>
2024-09-23 12:28:41 +0530

    first commit

---
+++ Sql task1.sql
@@ -0,0 +1,3 @@
+create table SQL_Task1 (Student_id int,"Student Name "varchar,Age int,"Gender" varchar,"City" varchar);
+select*from SQL_Task1
+copy SQL_Task1 from 'D:/sql task 1.csv' DELIMITER ',' csv header;
